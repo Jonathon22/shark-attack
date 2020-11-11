665bb45792626399ec0a0decb8945eb3936c577c
Jonathon Joyner <jonathonandon@gmail.com>
2020-11-10 21:11:13 -0600

    render students to dom

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import SharkTank from '../Components/SharkTank';
+import livingStudents from '../Components/LiveStudent';
 import './App.scss';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
+      <div>
+        <SharkTank livingStudents={livingStudents} />
       </div>
     );
   }
